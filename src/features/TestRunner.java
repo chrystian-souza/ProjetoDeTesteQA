4d35fbd1a1783b379297197f1c0580f5e27a591d
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-04-27 20:01:19 -0300

    commit3

--- src/features/TestRunner.java
+++ src/features/TestRunner.java
@@ -1,2 +1,13 @@
-package features;public class TestRunner {
+package features;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/features", glue = "")
+
+public class TestRunner {
+
+
 }
